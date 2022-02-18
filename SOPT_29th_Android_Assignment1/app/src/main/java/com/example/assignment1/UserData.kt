e3cc8483c055971ce7bcfd3eaf9b5aa62de04186
s9hn <tnghqn@naver.com>
2022-02-18 23:39:23 +0900

    [Feat] 2주차 과제 진행중 1. 프래그먼트 생성 2. 리사이클러뷰 생성

--- SOPT_29th_Android_Assignment1/app/src/main/java/com/example/assignment1/UserData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.assignment1
-
-data class UserData (
-    val name : String,
-    val introduction : String
-
-)
---
+++ app/src/main/java/com/example/week1/feature/home/HomeBindingAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.week1.feature.home
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+object HomeBindingAdapter {
+    @JvmStatic
+    @BindingAdapter("imgResId")
+    fun setImageResId(imageview: ImageView, resId: Int) {
+        imageview.setImageResource(resId)
+    }
+}

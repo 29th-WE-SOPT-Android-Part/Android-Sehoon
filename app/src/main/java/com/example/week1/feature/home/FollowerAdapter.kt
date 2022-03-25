5bcf6a1963fe4d103c573e9e7432b17d34b281a3
s9hn <tnghqn@naver.com>
2022-03-25 13:37:11 +0900

    PAUSE

--- app/src/main/java/com/example/week1/feature/home/FollowerAdapter.kt
+++ app/src/main/java/com/example/week1/feature/home/FollowerAdapter.kt
@@ -32,7 +32,7 @@ class FollowerAdapter : RecyclerView.Adapter<FollowerAdapter.FollowerViewHolder>
             binding.follower = data
             itemView.setOnClickListener() {
                 val name = binding.tvFollowerName.text.toString()
-q
+
 
 
 
